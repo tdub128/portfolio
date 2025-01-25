66564f3110b9f93b0a094a3730fd81267212e2b1
tdub128 <156718718+tdub128@users.noreply.github.com>
2025-01-25 02:24:30 -0800

    lab03.1

--- global.js
+++ global.js
@@ -16,10 +16,10 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 //       }
 
 let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'resume/', title: 'Resume' },
+    { url: '../', title: 'Home' },
+    { url: '../projects/', title: 'Projects' },
+    { url: '../contact/', title: 'Contact' },
+    { url: '../resume/', title: 'Resume' },
     { url: 'https://github.com/tdub128', title: 'GitHub' }
   ];
 
